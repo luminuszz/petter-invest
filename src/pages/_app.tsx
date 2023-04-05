972bb66f6cfc9f9eaf22c70bce8b2d1df958df4e
daviribeiro <luminuszz43@gmail.com>
2023-04-05 17:51:38 -0300

    initial experiments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider, ChakraTheme, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+} as ChakraTheme);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
